eaf30e61a37004c697dac2a91e8c694e6f8ebc6a
deama <deama@mail.com>
2019-12-04 16:09:33 +0000

    fixed bug in test file

--- tests/test_back_end.py
+++ tests/test_back_end.py
@@ -3,23 +3,41 @@ import unittest
 from flask import abort, url_for
 from flask_testing import TestCase
 from os import getenv
-from application import app, db
+from application import app, db, bcrypt
 from application.models import Player, Account_details, Scores
 
 
 class TestBase(TestCase):
 
     def create_app(self):
-        pass
+        config_name = "testing"
+        app.config.update(SQLALCHEMY_DATABASE_URI="mysql+pymysql://"+str(getenv("MYSQL_USER"))+":"+str(getenv("MYSQL_PASSWORD"))+"@"+str(getenv("MYSQL_HOST"))+"/"+str(getenv("MYSQL_DB_TEST")))
+        return app
 
     def setUp(self):
-        pass
+        db.session.commit()
+        db.drop_all()
+        db.create_all()
+
+        hashed_pw = bcrypt.generate_password_hash( "test" )
+
+        player = Player(name="testName")
+        db.session.add(player)
+
+        player_id = Player.query.filter_by( name="testName" ).first().player_id
+
+        account = Account_details( player_id=player_id, login="testName", password=hashed_pw )
+
+        # save users to database
+        db.session.add(account)
+        db.session.commit()
 
     def tearDown(self):
-        pass
+        db.session.remove()
+        db.drop_all()
 
 
-class Test_app(TestCase):
+class Test_app(TestBase):
 
     def test_homepage_view(self):
         response = self.client.get(url_for("home"))

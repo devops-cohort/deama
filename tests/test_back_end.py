b42198caf7e418d0f6361ce8c943f7143622e524
deama <deama@mail.com>
2019-12-04 16:34:55 +0000

    added more tests and report coverage for tests

--- tests/test_back_end.py
+++ tests/test_back_end.py
@@ -19,17 +19,6 @@ class TestBase(TestCase):
         db.drop_all()
         db.create_all()
 
-        hashed_pw = bcrypt.generate_password_hash( "test" )
-
-        player = Player(name="testName")
-        db.session.add(player)
-
-        player_id = Player.query.filter_by( name="testName" ).first().player_id
-
-        account = Account_details( player_id=player_id, login="testName", password=hashed_pw )
-
-        # save users to database
-        db.session.add(account)
         db.session.commit()
 
     def tearDown(self):
@@ -42,3 +31,27 @@ class Test_app(TestBase):
     def test_homepage_view(self):
         response = self.client.get(url_for("home"))
         self.assertEqual(response.status_code, 200)
+
+    def test_login_view(self):
+        target_url = url_for("snake")
+        redirect_url = url_for("login")
+        response = self.client.get(target_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, redirect_url)
+
+    def test_data_in_database(self):
+        hashed_pw = bcrypt.generate_password_hash( "test" )
+
+        player = Player(name="testName")
+        db.session.add(player)
+
+        player_id = Player.query.filter_by( name="testName" ).first().player_id
+
+        account = Account_details( player_id=player_id, login="testName", password=hashed_pw )
+
+        db.session.add(account)
+        db.session.commit()
+
+
+        player_name = Player.query.filter_by( name="testName").first().name
+        self.assertEqual( player_name, "testName" )
